ed6dd08a7e76fbe069095268860306b7ec1a4191
Rok Močnik <rok.mocnik@gmail.com>
2013-05-22 09:56:19 +0200

    Syntax corrections.

--- gw2api.py
+++ gw2api.py
@@ -9,54 +9,54 @@ except ImportError:
 
 def get_items():
     """ Get a list of all item ids. """
-    return _request("items.json")
+    return _request('items.json')
 
-def get_item_details(id, lang="en"):
+def get_item_details(id, lang='en'):
     """ Get details of specific item. """
-    return _request("item_details.json", item_id=id, lang=lang)
+    return _request('item_details.json', item_id=id, lang=lang)
 
 def get_recipes():
     """ Get a list of all recipes. """
-    return _request("recipes.json")
+    return _request('recipes.json')
 
-def get_recipe_details(id, lang="en"):
+def get_recipe_details(id, lang='en'):
     """ Get details of specific item. """
-    return _request("recipe_details.json", recipe_id=id, lang=lang)
+    return _request('recipe_details.json', recipe_id=id, lang=lang)
 
 def get_wvw_matches():
     """ Get the current running WvW matches. """
-    return _request("wvw/matches.json")
+    return _request('wvw/matches.json')
 
 def get_wvw_match_details(id):
     """ Get the current match details. """
-    return _request("wvw/match_details.json", match_id=id)
+    return _request('wvw/match_details.json', match_id=id)
 
-def get_wvw_objective_names(lang="en"):
+def get_wvw_objective_names(lang='en'):
     """ Get the names of all objectives in WvW maps. """
-    return _request("wvw/objective_names.json", lang=lang)
+    return _request('wvw/objective_names.json', lang=lang)
 
-def get_event_names(lang="en"):
+def get_event_names(lang='en'):
     """ Get names of all existing events. """
-    return _request("event_names.json", lang=lang)
+    return _request('event_names.json', lang=lang)
 
-def get_map_names(lang="en"):
+def get_map_names(lang='en'):
     """ Get names of all maps. """
-    return _request("map_names.json", lang=lang)
+    return _request('map_names.json', lang=lang)
 
-def get_world_names(lang="en"):
+def get_world_names(lang='en'):
     """ Get names of all world servers. """
-    return _request("world_names.json", lang=lang)
+    return _request('world_names.json', lang=lang)
 
 def get_events(**args):
     """ Get list events based on filtering by world, map and event. """
-    return _request("events.json", **args)
+    return _request('events.json', **args)
 
 def _request(json_location, **args):
     """ Makes a request on the Guild Wars 2 API."""
     url = 'https://api.guildwars2.com/v1/' + json_location + '?' + '&'.join(str(argument) + '=' + str(value) for argument, value in args.items())
     return json.loads(urllib2.urlopen(url).read())
 
-def get_enhanced_wvw_objective_names(lang="en"):
+def get_enhanced_wvw_objective_names(lang='en'):
     """ Get the real names of all WvW objectives, with their scores. """
     lang_table = {u'en': 0, u'de': 1, u'fr': 2, u'es': 3}
     return [dict(objective.items() +[(u'objective_name', misc.lang[objective[u'id']][lang_table[lang]])] ) for objective in misc.world]
